9dc8504f138ac15c2b4fde8da25497ca555ca694
Bertuğ Yılmaz <ylmazbertug@gmail.com>
2018-03-06 14:27:30 +0300

    Init linkedList sample playground

---
+++ LinkedList/MyPlayground.playground/Contents.swift
@@ -0,0 +1,40 @@
+// Created by Bertug YILMAZ
+// 06/03/2018
+//
+
+import UIKit
+import Foundation
+
+class Node {
+    var data: Int!
+    var next: Node!
+}
+
+class LinkedList {
+    private var size: Int = 0
+    private var head: Node!
+    
+    var listSize: Int {
+        get {
+            return size
+        }
+    }
+}
+
+extension LinkedList {
+    func insertFirst(value: Int){
+        
+    }
+    
+    func insertLast(value: Int){
+        
+    }
+    
+    func deleteFirst(){
+        
+    }
+    
+    func deleteLast(){
+        
+    }
+}
