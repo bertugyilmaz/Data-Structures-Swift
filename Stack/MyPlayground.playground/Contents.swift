b67b0ea7dcf8d69ffeedd10b0c3ef7a3a4f95ece
Bertuğ Yılmaz <ylmazbertug@gmail.com>
2018-03-15 13:31:44 +0300

    Added Person model class

--- Stack/MyPlayground.playground/Contents.swift
+++ Stack/MyPlayground.playground/Contents.swift
@@ -2,8 +2,39 @@
 // Created by Bertug YILMAZ
 // 10/03/2018
 //
-// Stack
+// Array Typed Stack
 
 import UIKit
 import Foundation
+
+class Person: NSObject {
+    
+    private var nameStr: String!
+    private var surnameStr: String!
+    private var ageStr: String!
+    
+    var name: String! {
+        get{
+            return nameStr
+        }set {
+            self.nameStr = newValue
+        }
+    }
+    
+    var surname: String! {
+        get{
+            return surnameStr
+        }set {
+            self.surnameStr = newValue
+        }
+    }
+    
+    var age: String! {
+        get{
+            return ageStr
+        }set{
+            self.ageStr = newValue
+        }
+    }
+}
 
